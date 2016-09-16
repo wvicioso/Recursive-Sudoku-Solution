e3aa57aea0738b180ea577691b2f48c0d49a38d3
Catherine Khuu & Nan Min & Wellington Vicioso <pair+catkhuu.nanutza.wvicioso@devbootcamp.com>
2016-09-16 12:37:07 -0400

    Create Sudoku class.

--- sudoku.rb
+++ sudoku.rb
@@ -1,22 +1,47 @@
-# Takes a board as a string in the format
-# you see in the puzzle file. Returns
-# something representing a board after
-# your solver has tried to solve it.
-# How you represent your board is up to you!
-def solve(board_string)
-end
+class Sudoku
+  attr_accessor :board
 
-# Returns a boolean indicating whether
-# or not the provided board is solved.
-# The input board will be in whatever
-# form `solve` returns.
-def solved?(board)
-end
 
-# Takes in a board in some form and
-# returns a _String_ that's well formatted
-# for output to the screen. No `puts` here!
-# The input board will be in whatever
-# form `solve` returns.
-def pretty_board(board)
+
+  def initialize(board_string)
+    @board = get_board(board_string)
+  end
+
+  # Takes a board as a string in the format
+  # you see in the puzzle file. Returns
+  # something representing a board after
+  # your solver has tried to solve it.
+  # How you represent your board is up to you!
+  def solve(board_string)
+    get_board(board_string)
+  end
+
+  # Returns a boolean indicating whether
+  # or not the provided board is solved.
+  # The input board will be in whatever
+  # form `solve` returns.
+  def solved?(board)
+  end
+
+  def get_board(board_string)
+    board_string.chars.each_slice(9).to_a
+  end
+
+  def get_cols(index)
+    board.transpose[index]
+  end
+
+  def get_row(index)
+    board[index]
+  end
+
+  def get_box
+  end 
+  # Takes in a board in some form and
+  # returns a _String_ that's well formatted
+  # for output to the screen. No `puts` here!
+  # The input board will be in whatever
+  # form `solve` returns.
+  def pretty_board(board)
+  end
 end
