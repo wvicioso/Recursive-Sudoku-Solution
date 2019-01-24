e0f4117579d0e2e3b95baafc03950eb1889cdfe4
wvicioso <wellington.vicioso@gmail.com>
2019-01-23 19:20:03 -0500

    remove unused methods

--- sudoku.rb
+++ sudoku.rb
@@ -1,4 +1,3 @@
-# require 'pry'
 
 class Sudoku
   attr_accessor :board, :solution
@@ -45,13 +44,11 @@ class Sudoku
     matrix[row_num].join
   end
 
-
   def find_super_box_index(value_index)
     box_row = value_index / 27
     box_col = value_index / 3 % 3
     STARTING_POINTS[[box_row,box_col]].values.first
   end
-
 
   def get_box(box_start, str)
     x = box_start
@@ -63,7 +60,6 @@ class Sudoku
     super_box
   end
 
-
   def current_possibles(idx, matrix, str)
     box = get_box(find_super_box_index(idx), str)
     row = get_rows(idx, matrix)
@@ -71,27 +67,6 @@ class Sudoku
     result = @possibles.split('').reject {|item| (box + row + col).split('').include?(item)}.join('')
     result
   end
-
-  def single_solution?(possible_nums)
-    possible_nums.length == 1
-  end
-
-  def more_than_one_solution(possible_nums)
-    possible_nums.length > 1
-  end
-
-  def guess_number(idx, possible_nums)
-    @board[idx] = possible_nums.split("").sample.to_s
-  end
-
-  def try_number(idx, possible_nums, num)
-    @board[idx] = possible_nums.split("")[num].to_s
-  end
-
-  def update_board(idx, possible_nums)
-      @board[idx] = possible_nums[0].to_s
-  end
-
 
   def pretty_board
     board_array = board_to_array(@solution)
@@ -128,13 +103,3 @@ class Sudoku
     end
 
 end
-
-
-# this = Sudoku.new("1-5  8-2  ---
-#                    -9-  -76 4-5
-#                    2--  4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--")
-
-# nan = this.find_super_box_index(2)
-
-
-# p this.get_box(nan)
